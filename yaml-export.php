7cbc27d025dedc61a0f5fc8d5a027e296b553412
ejonasson <ejonasson@gmail.com>
2014-05-09 11:08:31 -0400

    Added keyword search functionality

--- yaml-export.php
+++ yaml-export.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
- * Plugin Name: WP Multi-Export
+ * Plugin Name: WP Export to YAML
  * Plugin URI: https://zombo.com
- * Description: Setting to create an export HTML File and YAML for Posts and Pages
+ * Description: Setting to create an export YAML for Posts and Pages
  *
  * Author:      ASMBS
  * Author URI:  https://github.com/asmbs
@@ -12,29 +13,28 @@
  *
  * Version:     1.0.0-beta
  */
-
-
 class YAML_export {
 
-/* Construction of Class */
+	/* Construction of Class */
+
 	private static $_get_instance = NULL;
 	public function __construct(){
-	add_action('admin_init', function(){
-		$this->register_YAML_setting();
-	});
-	
-	add_action('admin_menu', function(){
-		$this->yaml_add_menu_page();	
-	});
-	
+		add_action('admin_init', function(){
+			$this->register_YAML_setting();
+		});
+
+		add_action('admin_menu', function(){
+			$this->yaml_add_menu_page();	
+		});
+
 	}
 	public function get_instance()
-    {
-        if (self::$_get_instance == NULL )
-            self::$_get_instance = new self;
+	{
+		if (self::$_get_instance == NULL )
+			self::$_get_instance = new self;
 
-        return self::$_get_instance;
-    }
+		return self::$_get_instance;
+	}
 	/* Add class to Menu */
 
 	public function yaml_add_menu_page(){
@@ -61,17 +61,16 @@ class YAML_export {
 
 	}
 
-
 // The query mamma jamma itself
 
 	public function execute_query(){
 //Make sure appropriate directories exist
-		 $directory = '/users/erik/query/outputs/';
+		$directory = '/users/erik/query_outputs/';
 		if (!is_dir($directory)){
 			mkdir($directory);
 			mkdir($directory . "/htmls");
 			mkdir($directory . "/mds");
-			mkdir($directory . "/yamlss");
+			mkdir($directory . "/yamls");
 			mkdir($directory . "/concats");
 
 		}
@@ -79,7 +78,8 @@ class YAML_export {
 		$args = array(
 			'post_type' => 'post', 'page',
 			'posts_per_page' => -1,
-		);
+			);
+		$keywords = $this->generate_keywords();
 		$the_query = new WP_Query($args);
 		if ($the_query->have_posts())
 		{
@@ -91,12 +91,12 @@ class YAML_export {
 				$single_cat = $categories[0]->cat_name;
 				$content = get_the_content();
 				$content = apply_filters('the_content', $content);
-				$generated_categories = $this->generate_categories($content);
-					$yaml_file = <<< EOT
+				$generated_categories = $this->generate_categories($content, $keywords);
+				$yaml_file = <<< EOT
 ----
 title: $title
 date: $date
-categories: $single_cat, $generated_categories
+categories: $single_cat, $generated_categories[0], $generated_categories[1], $generated_categories[2]
 ----
 
 EOT;
@@ -113,12 +113,58 @@ EOT;
 	}
 
 	//Category generation function
-	public function generate_categories($content){
-			return null;
+	public function generate_categories($content, $keywords){
+		/*
+		Take the list of Keywords
+		Count the number of occurrences of each
+		Merge the numbers for alternate titles
+		Identify the top three
+		return those as categories
+		*/
+		$categorycounts = array();
+		$topcategories = array();
+		$lcontent = strtolower($content);
+		foreach ($keywords as $keygroup){
+			$count = 0;
+			foreach($keygroup as $keyword){
+				$num = substr_count($lcontent, $keyword);
+				$count += $num;
+			}
+			$catarray = array(
+				'keyword' => $keygroup[0],
+				'count' => $count,
+				);
+			array_push($categorycounts, $catarray);
+		}
+		usort($categorycounts, function ($cat1, $cat2) {
+   		 return $cat2['count'] - $cat1['count'];
+		});
+		$categorycounts = array_slice($categorycounts, 0, 3);
+		var_dump($categorycounts);
+		foreach ($categorycounts as $category){
+			if ($category['count'] > 5){
+				array_push($topcategories, $category['keyword']);
+			}
+		}
+		return $topcategories;
+	}
+	public function generate_keywords(){
+		// CSVs should be formatted as such:
+		// The first item in a row is the "true keyword" - this is what you want to display
+		// The second and third items are "alternate keywords" - these are synonyms or alternate terms for the core keyword
+
+		$keywords = array();
+		$csv = fopen('/users/erik/asmbs_keywords.csv','r');
+		while (($data= fgetcsv($csv, 1000, ',')) !== FALSE){
+			$keyword = array($data[0], $data[1], $data[2]);
+			array_push($keywords, $keyword);
+		}
+		return $keywords;
 	}
 
 }
-
 YAML_export::get_instance();
+
+
 
 
